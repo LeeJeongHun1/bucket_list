870d778578eb878603f68de27520ff338c0a07c6
Lee Jeong Hun <wjdgns1155>
2018-06-05 15:54:00 +0900

    d

--- src/main/java/kr/co/bucket/Admin/controller/AdminController.java
+++ src/main/java/kr/co/bucket/Admin/controller/AdminController.java
@@ -1,10 +1,16 @@
 package kr.co.bucket.Admin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.co.bucket.Admin.service.AdminService;
+
 @Controller
 public class AdminController {
+	
+	@Autowired
+	private AdminService adminService;
 	
 	@RequestMapping("/admin/adminMem.do")
 	public String adminMemMove() {
---
+++ src/main/java/kr/co/bucket/repository/domain/AirSearch.java
@@ -0,0 +1,35 @@
+package kr.co.bucket.repository.domain;
+
+public class AirSearch {
+
+	private String arrivalDate; 
+	private String departureDate;
+	private String arrivalCity;
+	private String departureCity;
+	public String getArrivalDate() {
+		return arrivalDate;
+	}
+	public void setArrivalDate(String arrivalDate) {
+		this.arrivalDate = arrivalDate;
+	}
+	public String getDepartureDate() {
+		return departureDate;
+	}
+	public void setDepartureDate(String departureDate) {
+		this.departureDate = departureDate;
+	}
+	public String getArrivalCity() {
+		return arrivalCity;
+	}
+	public void setArrivalCity(String arrivalCity) {
+		this.arrivalCity = arrivalCity;
+	}
+	public String getDepartureCity() {
+		return departureCity;
+	}
+	public void setDepartureCity(String departureCity) {
+		this.departureCity = departureCity;
+	}
+	
+	
+}
